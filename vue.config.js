82e9182470147430c46fe1974bbe7037c7b68993
Joshua Hveem <jhveem@btech.edu>
2022-04-29 14:44:01 -0600

    attempting to fix reload issue

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,6 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
+  publicPath: '/var/www/html/meats-game',
   transpileDependencies: true,
   devServer: {
     allowedHosts: "meats-game.bridgetools.dev",
