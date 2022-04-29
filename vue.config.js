d8ecffa35d3c80f8de8165fd62b7422b430e93b8
Joshua Hveem <jhveem@btech.edu>
2022-04-29 10:02:47 -0600

    disable host check

--- vue.config.js
+++ vue.config.js
@@ -2,7 +2,7 @@ const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
   devServer: {
-    public: "https:meats-game.bridgetools.dev",
+    allowedHosts: "meats-game.bridgetools.dev",
     host: "localhost",
     port: 3015
   }
